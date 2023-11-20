4b1740c0115a62cdec21878b59241f7d15f50c8f
vladkolyada <v.kolyada7.13@gmail.com>
2023-11-20 11:07:04 +0200

    change code only for sqlit3

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from aiogram import executor
 
-from loader import dp, db
+from loader import dp
 import middlewares, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -12,12 +12,6 @@ async def on_startup(dispatcher):
 
     await set_default_commands(dispatcher)
     # set default commands
-
-    try:
-        await db.create_table_users()
-    except Exception as err:
-        print(err)
-    # creates a data base
 
 
 if __name__ == '__main__':
--- loader.py
+++ loader.py
@@ -2,12 +2,10 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
-from utils.db_api.database import DataBase
 from utils.db_api.data_base_sqlite import DataBaseSqlite
 
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-db = DataBase()
 db_sqlite = DataBaseSqlite("utils/db_api/datab.db")
