e429f0f11014a2bea513f5f15eb09e3412d50c7e
vladkolyada <v.kolyada7.13@gmail.com>
2023-11-20 21:55:16 +0200

    added new image

--- keyboards/inline/default_inline_keyboards.py
+++ keyboards/inline/default_inline_keyboards.py
@@ -8,9 +8,11 @@ start_keyboard = InlineKeyboardMarkup(inline_keyboard=[
 
 social_medias_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton(text="Youtube", url="https://www.youtube.com/@Dinosaur2023")
+        InlineKeyboardButton(text="Youtube", url="https://www.youtube.com/@Dinosaur2023"),
+        InlineKeyboardButton(text="Instagram", url="https://instagram.com/timko.sergii?igshid=aXo4OTY4a3Z2dTZq")
     ],
     [
-        InlineKeyboardButton(text="Instagram", url="https://instagram.com/timko.sergii?igshid=aXo4OTY4a3Z2dTZq")
+        InlineKeyboardButton(text="Замовити приїзд проєкту в навчальний заклад",
+                             url="https://www.instagram.com/direct/t/17848766366949304")
     ]
 ])
